43ebc3862fe8165fccf55460e102f95bfc218bf5
framartin <martin.gubri@framasoft.org>
2017-10-01 01:04:53 -0300

    update draft

--- transfer_logistic_clean.py
+++ transfer_logistic_clean.py
@@ -16,7 +16,9 @@ res = glm_binom.fit()
 print(res.summary())
 
 # example to perturbate
-x_0 = data.exog[4,:]
+#x_0 = data.exog[4,:]
+#x_0 = data.exog[6,:]
+x_0 = data.exog[1,:]
 # >>> res.predict(data.exog[4,:])
 # array([ 0.32251021])
 # >>> res.predict(data.exog[4,:], linear=True)
@@ -42,12 +44,15 @@ eps = compute_adv_pertubation(x_0, beta_hat, beta_hat_var, idx_beta_0)
 
 preds = [(x + compute_adv_pertubation(x, beta_hat, beta_hat_var, idx_beta_0)).dot(beta_hat) for x in data.exog]
 
-def solve_lambda(alpha, x, beta_hat, eps, tolerance = 1e-8, verbose = False):
+def solve_lambda(alpha, x, beta_hat, eps, tolerance = 1e-10, verbose = False):
+    if verbose:
+        print('-----------')
     d = math.sqrt(2)*special.erfinv(2*alpha-1)
     A = np.outer(beta_hat, beta_hat) - d**2 * var_covar_matrix 
     a = eps.dot(A).dot(eps)
     b = x.dot(A).dot(eps) + eps.dot(A).dot(x)
     c = x.dot(A).dot(x)
+    #import pdb; pdb.set_trace()
     if verbose:
         print('value a: {0}'.format(a))
     delta = b**2 - 4*a*c
@@ -64,16 +69,35 @@ def solve_lambda(alpha, x, beta_hat, eps, tolerance = 1e-8, verbose = False):
         lambda2 = (-b + delta**0.5) / (2*a)
         if verbose:
             print('Two solutions: {0}, {1}'.format(lambda1, lambda2))
-        for lambda_star in [lambda1, lambda2]:
+        for lambda_star in [lambda2, lambda1]: # TODO: verifier que resiste a l'ordre
             x_adv = x+lambda_star*eps
             eq = abs(x_adv.dot(beta_hat) + d*math.sqrt( x_adv.dot(var_covar_matrix).dot(x_adv)))
-            print(eq)
+            # TODO: on est loin
+            print('Value eq: {0}'.format(eq))
+            eq2 = abs(x_adv.dot(A).dot(x_adv))
+            print('Value eq2: {0}'.format(eq2))
+            print('--')
             if eq < tolerance:
                 return lambda_star
+        #import pdb; pdb.set_trace()
         raise ValueError('Error when solving the 2nd degres eq')
 
-solve_lambda(alpha, x_0, beta_hat, eps, verbose = True)
-solve_lambda(0.3, x_0, beta_hat, eps, verbose = True)
+#solve_lambda(alpha, x_0, beta_hat, eps, verbose = True)
+#solve_lambda(0.3, x_0, beta_hat, eps, verbose = True)
+#solve_lambda(0.7, x_0, beta_hat, eps, verbose = True)
+
+
+def plot_alpha_lambda(x, min_alpha=0.001, max_alpha=1, step=0.01):
+    eps = compute_adv_pertubation(x, beta_hat, beta_hat_var, idx_beta_0)
+    alpha_range = np.arange(min_alpha, max_alpha, step)
+    y_lambda = [solve_lambda(alpha, x, beta_hat, eps) for alpha in alpha_range]
+    plt.plot(alpha_range, y_lambda, 'r--')
+    plt.show()
+
+#plot_alpha_lambda(x_0)
+#plot_alpha_lambda(x_0, min_alpha = 0.001, max_alpha=0.5, step = 0.001)
+plot_alpha_lambda(x_0, min_alpha = 0.5, max_alpha=0.999, step = 0.001)
+
 
 
 #def f(alpha, x_0 = x_0, beta_hat_0 = beta_hat_0, var_covar_matrix = var_covar_matrix, c = c):
@@ -89,55 +113,3 @@ solve_lambda(0.3, x_0, beta_hat, eps, verbose = True)
 #         plt.axhline(y = d)
 #     plt.show()
 
-
-
-d = -math.sqrt(2)*special.erfinv(2*alpha-1)
-
-#plot_f(hline = d)
-#plot_f(0.0, 0.05, 0.0000001)
-
-def solve_lambda(beta_hat, x_0, eps, d, var_covar_matrix, verbose=True):
-    A = (eps.dot(beta_hat))**2 - d**2 * ( eps.dot(var_covar_matrix).dot(eps) )
-    #A = np.dot(eps, beta_hat) ** 2 - d ** 2 * np.dot(np.dot(eps, var_covar_matrix), eps)
-    B = 2 * np.dot(x_0, beta_hat) * np.dot(eps, beta_hat) - d ** 2 * ( np.dot(np.dot(x_0, var_covar_matrix), eps) + np.dot(np.dot(eps, var_covar_matrix), x_0) )
-    C = np.dot(x_0,  beta_hat) ** 2 - d ** 2 * np.dot(np.dot(x_0, var_covar_matrix), x_0)
-    if verbose:
-        print('value A: {0}'.format(A))
-    delta = B ** 2 - 4 * A * C
-    if delta < 0:
-        if verbose:
-            print('No solution')
-        return None
-    elif delta == 0:
-        if verbose:
-            print('One solution')
-        return -B/(2*A)
-    elif delta > 0:
-        lambda1 = (-B - delta**0.5) / (2*A)
-        lambda2 = (-B + delta**0.5) / (2*A)
-        if verbose:
-            print('Two solutions: {0}, {1}'.format(lambda1, lambda2))
-
-        return(max(lambda1, lambda2))
-
-lambda_star = solve_lambda(beta_hat, x_0, eps, d, var_covar_matrix)
-print(lambda_star)
-
-## values of lambda VS alpha 
-
-def compute_lambda(alpha, beta_hat_0, x_0, eps, var_covar_matrix, verbose=False):
-    d = -2*special.erfinv(2*alpha-1)
-    lambda_star = solve_lambda(beta_hat, x_0, eps, d, var_covar_matrix, verbose=verbose)
-    return lambda_star
-
-def plot_alpha_lambda(min_a = 0.001, max_a=1, step = 0.01):
-    alpha_range = np.arange(min_a, max_a, step)
-    y_lambda = [compute_lambda(alpha, beta_hat, x_0, eps, var_covar_matrix) for alpha in alpha_range]
-    plt.plot(alpha_range, y_lambda, 'r--')
-    plt.show()
-
-#plot_alpha_lambda()
-plot_alpha_lambda(min_a = 0.001, max_a=0.5, step = 0.001)
-
-
-compute_lambda(0.004, beta_hat, x_0, eps, var_covar_matrix, verbose = True)
