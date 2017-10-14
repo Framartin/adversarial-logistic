8e48aab5d48a5fe19feb38728cb106728b297d98
framartin <martin.gubri@framasoft.org>
2017-10-14 02:04:02 -0300

    fix important bug in solve_alpha() + other fixes + improvements

--- adversarialLogistic.py
+++ adversarialLogistic.py
@@ -15,6 +15,7 @@ class AdversarialLogistic(object):
 
     def __init__(self, model, X_train=None, lower_bound=float('-inf'), upper_bound=float('inf')):
         super(AdversarialLogistic, self).__init__()
+        #TODO:
         # check if model is supported
         # check that not multinomial logit
         # check that we use the bernoouilli Logit, not the Binomial
@@ -73,7 +74,7 @@ class AdversarialLogistic(object):
             # There is a constant
             return temp[0]
         elif temp.shape[0] > 1:
-            raise ValueError('There is at least 2 contant features in X_train.')
+            raise ValueError('There is at least 2 constant features in X_train.')
         else:
             return None
 
@@ -87,16 +88,17 @@ class AdversarialLogistic(object):
                 # statsmodels GLM computes the covariance matrix
                 self.cov_params = self.model.normalized_cov_params
             else:
-                # statsmodels do not support the cov matrix for regularized GLM 
+                # statsmodels do not support the computation of the cov matrix for regularized GLM 
                 raise ValueError('Model not supported yet.')
         elif self.module == 'sklearn':
             if self.model.get_params()['C']>=1e10: # Logit without regularization
                 assert(X_train is not None)
                 yhat = self.model.predict_proba(X_train_origin)[:,self.model.classes_==1]
+                del X_train_origin
                 W = np.diag((yhat*(1-yhat)).squeeze())
                 Xt_W_X = X_train.T.dot(W).dot(X_train)
-                unrestricted_cov_params = np.linalg.inv(Xt_W_X)
-                self.cov_params = unrestricted_cov_params
+                del X_train
+                self.cov_params = np.linalg.inv(Xt_W_X) # unrestricted Var(beta)
             elif self.model.get_params()['penalty']=='l2': #L2 Regularized Logit
                 assert(X_train is not None and y_train is not None)
                 # we need to fit the unregularized logit to have an estimate of Omega(beta_0)
@@ -104,6 +106,7 @@ class AdversarialLogistic(object):
                 # An hacky solution is to set C to a very high value. Note that the 
                 # statsmodels implementation is too slow for some ML datasets
                 # See: https://github.com/scikit-learn/scikit-learn/issues/6738
+                # Question: is there a better way to compute Var(beta) without the unrestricted estimation?
                 sklearn_params = self.model.get_params()
                 sklearn_params['C'] = 1e12
                 unregularized_model = linear_model.LogisticRegression()
@@ -112,8 +115,10 @@ class AdversarialLogistic(object):
                 if unregularized_model.n_iter_ >= sklearn_params['max_iter']:
                     print('Warning! Max number of iterations reached. May not be optimal.')
                 yhat_ur = unregularized_model.predict_proba(X_train_origin)[:,unregularized_model.classes_==1]
+                del X_train_origin
                 W = np.diag((yhat_ur*(1-yhat_ur)).squeeze())
                 Xt_W_X = X_train.T.dot(W).dot(X_train)
+                del X_train
                 lambda_c = 1.0/self.model.get_params()['C']
                 invOmegaLambda = np.linalg.inv(Xt_W_X + 2*lambda_c*np.identity(Xt_W_X.shape[0]))
                 self.cov_params = invOmegaLambda.dot(Xt_W_X).dot(invOmegaLambda)
@@ -151,25 +156,24 @@ class AdversarialLogistic(object):
         a = delta.dot(A).dot(delta)
         b = x.dot(A).dot(delta) + delta.dot(A).dot(x)
         c = x.dot(A).dot(x)
-        #import pdb; pdb.set_trace()
         if verbose:
             print('value a: {0}'.format(a))
-        delta = b**2 - 4*a*c
-        if abs(delta) < tol: # delta == 0 
+        DeltaEq2 = b**2 - 4*a*c
+        if abs(DeltaEq2) < tol: # DeltaEq2 == 0 
             if verbose:
                 print('One solution')
             return -b/(2*a)
-        elif delta < 0:
+        elif DeltaEq2 < 0:
             if verbose:
-                print('No real solution. Delta: {0}'.format(delta))
+                print('No real solution. Delta: {0}'.format(DeltaEq2))
             return None
-        elif delta > 0:
-            lambda1 = (-b - delta**0.5) / (2*a)
-            lambda2 = (-b + delta**0.5) / (2*a)
+        elif DeltaEq2 > 0:
+            lambda1 = (-b - DeltaEq2**0.5) / (2*a)
+            lambda2 = (-b + DeltaEq2**0.5) / (2*a)
             if verbose:
                 print('Two solutions: {0}, {1}'.format(lambda2, lambda1))
             for lambda_star in [lambda1, lambda2]: #[lambda1, lambda2]: # TODO: verifier que resiste a l'ordre
-                x_adv = x+lambda_star*delta
+                x_adv = x + lambda_star*delta
                 eq = abs(x_adv.dot(beta_hat) + d*math.sqrt( x_adv.dot(self.cov_params).dot(x_adv)))
                 if verbose:
                     print('Value eq: {0}'.format(eq))
@@ -180,19 +184,37 @@ class AdversarialLogistic(object):
                     if verbose:
                         print('----')
                     return lambda_star
-        raise ValueError('Error when solving the 2nd degres eq')
+        raise ValueError('Error when solving the 2nd degree equation.')
 
-    def plot_lambda_vs_alpha(self, x, min_alpha=0.001, max_alpha=0.999, step=0.01, tol=1e-6, verbose=False):
+    def plot_lambda_vs_alpha(self, x, alpha_min=0.001, alpha_max=0.999, step=0.01, tol=1e-6, verbose=False):
         if not (hasattr(self, 'cov_params')):
             raise Exception('Missing cov_params. Call: self.compute_covariance(X_train, y_train)')
         x = self.add_constant(x=x)
         delta = self.compute_orthogonal_projection(x)
-        alpha_range = np.arange(min_alpha, max_alpha, step)
+        alpha_range = np.arange(alpha_min, alpha_max, step)
         lambdas = [self.solve_lambda(alpha, x, delta, tol=tol, verbose=verbose) for alpha in alpha_range]
         plt.plot(alpha_range, lambdas, 'r--')
         plt.show()
 
-    def compute_adversarial_perturbation(self, x, y, alpha = 0.05, X_train=None, y_train=None, tol=1e-6, verbose=False):
+    def check_bounds(self, x_adv, out_bounds):
+        assert(out_bounds in ['clipping', 'missing', 'nothing'])
+        if np.any(x_adv < self.lower_bound):
+            print('Adversarial example x_adv < lower_bound.')
+            if out_bounds == 'missing':
+                return None
+            elif out_bounds == 'clipping':
+                x_adv[x_adv < self.lower_bound] = self.lower_bound
+        if np.any(x_adv > self.upper_bound):
+            print('Adversarial example x_adv > upper_bound.')
+            if out_bounds == 'missing':
+                return None
+            elif out_bounds == 'clipping':
+                x_adv[x_adv > self.upper_bound] = self.upper_bound
+        return x_adv
+
+    def compute_adversarial_perturbation(self, x, y, alpha = 0.05, out_bounds='nothing', tol=1e-6, verbose=False):
+        # param out_bounds: 'clipping' or 'missing' or 'nothing'
+        #TODO: check sign(y) -> value of alpha
         #TODO: add tol to check range
         #TODO: add clipping param
         x = self.add_constant(x=x)
@@ -201,15 +223,13 @@ class AdversarialLogistic(object):
         delta = self.compute_orthogonal_projection(x)
         x_adv_0 = x + delta
         # check range of x_adv_0
-        if np.any(np.logical_or(x_adv_0 < self.lower_bound, x_adv_0 > self.upper_bound)):
-            raise ValueError('Adversarial example x_adv_0 outside bounds.')
+        x_adv_0 = self.check_bounds(x_adv_0, out_bounds)
         # check pred(x_adv_0)
         assert(np.sign(x_adv_0.dot(self.beta_hat)) != np.sign(y))
         lambda_star = self.solve_lambda(alpha, x, delta, tol=tol, verbose=verbose)
-        x_adv = x + lambda_star * delta
-        # check range of x_adv
-        if np.any(np.logical_or(x_adv < self.lower_bound, x_adv > self.upper_bound)):
-            raise ValueError('Adversarial example x_adv_star outside bounds.')
-        # check pred(x_adv)
-        assert(np.sign(x_adv.dot(self.beta_hat)) != np.sign(y))
-        return {'lambda_star': lambda_star, 'x_adv': x_adv}
+        x_adv_star = x + lambda_star * delta
+        # check range of x_adv_star
+        x_adv_star = self.check_bounds(x_adv_star, out_bounds)
+        # check pred(x_adv_star)
+        assert(np.sign(x_adv_star.dot(self.beta_hat)) != np.sign(y))
+        return {'lambda_star': lambda_star, 'x_adv_star': x_adv_star, 'x_adv_0': x_adv_0}
